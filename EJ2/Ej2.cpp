54a14a392316158614b3957f9a01f0ecd3904a3f
Sabil667 <msabi@myuax.com>
2023-11-23 12:37:36 +0100

    .

--- EJ2/Ej2.cpp
+++ EJ2/Ej2.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <string>
 
+
 int main(){
 
+    auto sentence = std::string("OpenAI is a leading AI research lab.");
+    std::cout<<"La frase es: "<<sentence<<std::endl;
+
+
+    std::cout<<"Tamano de la cadena: "<<sentence.size()<<std::endl;
+    std::cout<<"Esta la cadena vacia? "<<(sentence.empty() ?"Si" : "No")<<std::endl;
 
 
 }
