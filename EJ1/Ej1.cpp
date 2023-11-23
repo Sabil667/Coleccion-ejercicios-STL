75826e02d48b6b7bdb1da43cb0684fec5cc708be
Sabil667 <msabi@myuax.com>
2023-11-23 12:17:00 +0100

    .

---
+++ EJ1/Ej1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+    cout<<"Introduce una frase: ";
+
+    string frase;
+    getline(cin, frase);
+
+    cout<<"La frase introducida es: "<<frase<<endl;
+
+
+
+}
